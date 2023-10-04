2a1ef170aaf5ab87f8e8057cc46e5b9e6ad04631
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-05 01:44:17 +0200

    add appcourses component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,7 @@
 <script>
 import AppWorkCulture from './AppWorkCulture.vue';
-import AppSkills from './AppSkills.vue'
+import AppSkills from './AppSkills.vue';
+import AppCourses from './AppCourses.vue'
 
 
 export default {
@@ -9,6 +10,7 @@ export default {
     components: {
         AppWorkCulture,
         AppSkills,
+        AppCourses,
     }
 
 }
@@ -19,105 +21,9 @@ export default {
 
         <AppSkills />
 
-        <section class="courses">
-            <h6 class="text-center my_grey">
-                JOIN MAXCOACH AT BEST
-            </h6>
-            <h1 class="text-center fw-semibold">
-                Latest Online <span class="my_green fw-normal">Courses</span>
-            </h1>
-            <div class="row my-5">
-                <div class="col-4">
-                    <div class="card border-0">
-                        <!-- card image -->
-                        <img src="../assets/img/course-02-480x298.jpg" alt="">
-                        <div class="card-body px-4 pt-4">
-                            <!-- price -->
-                            <span class="my_green fs-4 fw-bold">
-                                $40.<span class="fs-5">00</span>
-                            </span>
-                            <!-- course type -->
-                            <a href="#">
-                                <h4 class="fw-semibold py-3">
-                                    Learning to Write as a Professional Author
-                                </h4>
-                            </a>
-                            <!-- info about students and lessons -->
-                            <div class="card_info d-flex">
-                                <div class="">
-                                    <i class="fa-regular fa-file-lines"></i>
-                                    <span class="ps-2">20 Lessons</span>
-                                </div>
-                                <div class="ps-4">
-                                    <i class="fa-regular fa-user"></i>
-                                    <span class="ps-2">50 Students</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card border-0">
-                        <!-- card image -->
-                        <img src="../assets/img/stock-full-hd-03-480x298.jpg" alt="">
-                        <div class="card-body px-4 pt-4">
-                            <!-- price -->
-                            <span class="my_green fs-4 fw-bold">
-                                $0.<span class="fs-5">00</span>
-                            </span>
-                            <!-- course type -->
-                            <a href="#">
-                                <h4 class="fw-semibold py-3">
-                                    Customer-centric Info-Tech Strategies
-                                </h4>
-                            </a>
-                            <!-- info about students and lessons -->
-                            <div class="card_info d-flex">
-                                <div class="">
-                                    <i class="fa-regular fa-file-lines"></i>
-                                    <span class="ps-2">24 Lessons</span>
-                                </div>
-                                <div class="ps-4">
-                                    <i class="fa-regular fa-user"></i>
-                                    <span class="ps-2">769 Students</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card border-0">
-                        <!-- card image -->
-                        <img src="../assets/img/stock-full-hd-04-480x298.jpg" alt="">
-                        <div class="card-body px-4 pt-4">
-                            <!-- price -->
-                            <span class="my_green fs-4 fw-bold">
-                                $19.<span class="fs-5">00</span>
-                            </span>
-                            <!-- course type -->
-                            <a href="#">
-                                <h4 class="fw-semibold py-3">
-                                    Open Programming Courses for Everyone: Python
-                                </h4>
-                            </a>
-                            <!-- info about students and lessons -->
-                            <div class="card_info d-flex">
-                                <div class="">
-                                    <i class="fa-regular fa-file-lines"></i>
-                                    <span class="ps-2">17 Lessons</span>
-                                </div>
-                                <div class="ps-4">
-                                    <i class="fa-regular fa-user"></i>
-                                    <span class="ps-2">62 Students</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <AppCourses />
 
-            </div>
 
-        </section>
 
     </main>
 </template>
@@ -125,33 +31,4 @@ export default {
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables.scss' as *;
-
-h1 {
-    font-size: 3rem;
-}
-
-img {
-    border-top-right-radius: 10px;
-    border-top-left-radius: 10px;
-}
-
-.card_info {
-    color: $coach-grey;
-}
-
-.card {
-
-    a {
-        text-decoration: none;
-    }
-
-    h4 {
-        color: black;
-    }
-
-}
-
-.card:hover h4 {
-    color: $coach-green;
-}
 </style>
