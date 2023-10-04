14477e0abfe82e29b76c001bee413055ff4f4bad
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-04 13:26:38 +0200

    add work culture section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,10 +6,11 @@ export default {
 <template>
     <main class="container my_width">
         <section class="work_culture">
+            <!-- top row -->
             <div class="row">
                 <!-- column left with paragraphs -->
                 <div class="col-4 pt-5">
-                    <h6 class="my_grey">
+                    <h6 class="my_grey pt-5">
                         Everything in <span class="fw-bolder">MAXCOACH</span>
                     </h6>
                     <p class="fs-1 fw-semibold lh-sm">
@@ -32,8 +33,46 @@ export default {
                         alt="" class="w-50 opacity-50">
                 </div>
                 <!-- right column images-->
-                <div class="col-6 my_bg_img my_pt position-relative">
-                    <img src="../assets/img/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg" alt="">
+                <div class="col-6 my_bg_img pt-5 position-relative">
+                    <!-- main image -->
+                    <div class="pt-5">
+                        <img src="../assets/img/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg" alt="">
+                    </div>
+                    <!-- top right image  -->
+                    <div class="image_abs">
+                        <img src="../assets/img/about-us-01-ohkia3qg98rq9vg3s0mn2hkflr1migdqdw370qnegc.jpg" alt=""
+                            class="rounded-3">
+                    </div>
+                    <!-- bottom circle absolute -->
+                    <div class="circle_abs">
+                        <img src="../assets/img/maxcoach-shape-02.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center my_mt">
+                <div class="col-3 text-center">
+                    <h2 class="my_green fw-bold">
+                        129+
+                    </h2>
+                    <p class="my_grey fw-semibold">
+                        ENROLLED LEARNERS
+                    </p>
+                </div>
+                <div class="col-3 text-center">
+                    <h2 class="my_green fw-bold">
+                        80
+                    </h2>
+                    <p class="my_grey fw-semibold">
+                        FINISHED SESSIONS
+                    </p>
+                </div>
+                <div class="col-3 text-center">
+                    <h2 class="my_green fw-bold">
+                        4%
+                    </h2>
+                    <p class="my_grey fw-semibold">
+                        SATISFACTION RATE
+                    </p>
                 </div>
             </div>
         </section>
@@ -45,18 +84,35 @@ export default {
 @use '../assets/scss/partials/variables.scss' as *;
 
 .work_culture {
-    margin-top: 8rem;
+    margin-top: 5rem;
     margin-bottom: 8rem;
 
+    /* top row */
     .my_bg_img {
         background-image: url(../assets/img/maxcoach-shape-07-150x150.png);
         background-repeat: no-repeat;
         background-position: top 0% right 20%;
     }
 
-    .my_pt {
-        padding-top: 4rem;
+    .image_abs {
+        position: absolute;
+        top: - 5%;
+        right: 0;
     }
+
+    .circle_abs {
+        position: absolute;
+        bottom: - 15%;
+        right: 20%;
+    }
+
+    /* bottom row */
+
+    .my_mt {
+        margin-top: 8rem;
+    }
+
+
 
 }
 </style>
